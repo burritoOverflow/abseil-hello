dc26ce77450ebc902f542f4217f52d95c5560745
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-08-18 15:45:13 -0400

    Add logging to example.

--- cmake-hello-install-binaries/src/main.cc
+++ cmake-hello-install-binaries/src/main.cc
@@ -1,8 +1,10 @@
-#include <iostream>
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
+#include <iostream>
 
-int main(int argc, const char** argv) {
-	for (int i = 0; i < argc; ++i) {
-		std::cout << absl::StrCat("Arg ", i, ": ", argv[i], "\n");
-	}
+int main(int argc, const char **argv) {
+  for (int i = 0; i < argc; ++i) {
+    LOG(INFO) << absl::StrCat("Arg ", i, ": ", argv[i], "\n");
+  }
 }
